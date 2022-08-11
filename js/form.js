6e94007d20e699b8518f041b3fe2cf531d28b523
Alexey Demyanov <deal110988@him-mail.ch>
2022-08-11 18:00:06 +0200

    Выполнены задания 7.15, 7.16 и 8.19

--- js/form.js
+++ js/form.js
@@ -3,6 +3,58 @@ const adFormFieldsetElements = adFormElement.querySelectorAll('fieldset');
 const mapFiltersFormElement = document.querySelector('.map__filters');
 const mapFeaturesElement = mapFiltersFormElement.querySelector('.map__features');
 const mapFiltersFormSelectElements = mapFiltersFormElement.querySelectorAll('select');
+const roomNumberFormElement = adFormElement.querySelector('#room_number');
+const capacityElement = adFormElement.querySelector('#capacity');
+const accommodationTypeElement = adFormElement.querySelector('#type');
+const priceElement = adFormElement.querySelector('#price');
+
+const validateForm = () => {
+  const pristine = new Pristine(adFormElement);
+
+  pristine.addValidator(roomNumberFormElement, (value) => {
+    if (value === '1' && capacityElement.value === '1') {
+      return true;
+    }
+    if (value === '2' && capacityElement.value === '1' || value === '2' && capacityElement.value === '2') {
+      return true;
+    }
+    if (value === '3' && capacityElement.value !== '0') {
+      return true;
+    }
+    if (value === '100' && capacityElement.value === '0') {
+      return true;
+    }
+    return false;
+  }, 'error', 1, false);
+
+  pristine.addValidator(accommodationTypeElement, (value) => {
+    if (value === 'bungalow') {
+      priceElement.min = '0';
+    }
+    if (value === 'flat') {
+      priceElement.min = '1000';
+    }
+    if (value === 'hotel') {
+      priceElement.min = '3000';
+    }
+    if (value === 'house') {
+      priceElement.min = '5000';
+    }
+    if (value === 'palace') {
+      priceElement.min = '10000';
+    }
+    priceElement.placeholder = priceElement.min;
+    return true;
+  }, 'error', 2, false)
+
+  adFormElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formIsValid = pristine.validate();
+    if (formIsValid) {
+      adFormElement.submit();
+    }
+  });
+};
 
 const makePageInactive = () => {
   adFormElement.classList.add('ad-form--disabled');
@@ -30,4 +82,4 @@ const makePageActive = () => {
   });
 };
 
-export { makePageInactive, makePageActive };
+export { makePageInactive, makePageActive, validateForm };
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { getCardData, renderOfferCard } from './get-offers.js';
-import { makePageInactive, makePageActive } from './form.js';
+import { makePageInactive, makePageActive, validateForm } from './form.js';
 
 const cardData = getCardData();
 renderOfferCard(cardData);
 
 makePageInactive();
-
 makePageActive();
+validateForm();
